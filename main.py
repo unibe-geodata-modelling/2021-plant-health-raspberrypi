f519c6694574c8782a3a98bdd8d221ba76a9426c
networkscientist <71866336+networkscientist@users.noreply.github.com>
2021-10-20 20:51:04 +0200

    No notable changes.

--- main.py
+++ main.py
@@ -3,10 +3,15 @@
 import moisture
 import datetime
 import dht22
+import plotter
 
 gpio = 4 # BCM Numbering
 
-chan0 = moisture.initialize()
+##chan0 = moisture.initialize()
 ##moisture.read_moisture(chan0)
-dht_sensor = dht22.dht_init(gpio)
+
+# Initialize the dht22 sensor. Pass the gpio number and get the sensor object and the dataframe.
+dht_sensor,df = dht22.dht_init(gpio)
+##print(dht22.dht_reader(dht_sensor))
 #print (datetime.datetime.now())
+##dht22.dht22_multi_measure(dht_sensor,df, 2)
