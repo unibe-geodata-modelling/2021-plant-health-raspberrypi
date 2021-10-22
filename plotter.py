af714004068ea5a8308cbc7cd74fc2d16db4239d
networkscientist <71866336+networkscientist@users.noreply.github.com>
2021-10-22 16:52:54 +0200

    Reading from the CSV and plotting the DataFrame with pyplot is now possible, though the dates are in the wrong unit.

--- plotter.py
+++ plotter.py
@@ -10,8 +10,25 @@ def plotter(csv_path):
     """
     try:
         # Read the csv file into a dataframe. The separator is defined with sep and can be written in Regex.
-        df = pd.read_csv(csv_path, sep='\s+', usecols=["temp_c"])
-        plt.plot(df)
+        df = pd.read_csv(csv_path, sep='\s+')
+        df['time'] = pd.to_datetime(df['time'])
+    
+##        plt.plot(df, x="time", y="temp_c", kind="scatter")
+        df.info()
+        # Create figure and plot space
+        fig, ax = plt.subplots(figsize=(10, 10))
+
+        # Add x-axis and y-axis
+        ax.scatter(df.index.values,
+                   df['temp_c'],
+                   color='purple')
+
+        # Set title and labels for axes
+        ax.set(xlabel="Time",
+               ylabel="Temp C",
+               title="Temperature Plot")
+##        plt.scatter(df["temp_c"])
         plt.show()
     except:
+        df.info()
         print("An error has occured.")
