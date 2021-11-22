50f6cb2f9baa97d4fa318de2fbf6313db65ae816
networkscientist <71866336+networkscientist@users.noreply.github.com>
2021-11-22 16:32:56 +0100

    removed the soil moisture calls from the main script, since it has not yet been fully integrated.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 ## Imports
 import pandas as pd
-import moisture
 import datetime
 import dht22
 import plotter
@@ -10,13 +9,8 @@ import plotter
 ## Initial Values
 gpio = 4 # BCM Numbering
 
-##chan0 = moisture.initialize()
-##moisture.read_moisture(chan0)
-
 ## Main Script
 # Initialize the dht22 sensor. Pass the gpio number and get the sensor object and the dataframe.
 dht_sensor,df = dht22.dht_init(gpio)
-##print(dht22.dht_reader(dht_sensor))
-#print (datetime.datetime.now())
-df = dht22.dht22_multi_measure(dht_sensor,df,200)
+df = dht22.dht22_multi_measure(dht_sensor,df,2000)
 plotter.plotter()
